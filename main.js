50fb550860311af9359ed1378ad50a8cfd9822c6
tania <tani.zhelezniak@gmail.com>
2018-08-20 13:49:43 +0300

    prototype

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 $(function () {
 
     class Animal {
@@ -20,13 +22,92 @@ $(function () {
             this.col = "white";
         }
     }
-    var cat = new Cat(100);
-    var dog = new Dog(400);
+    let cat = new Cat(100);
+    let dog = new Dog(400);
 
     console.log(cat.age);
     console.log(dog.age);
 
+     const test = 50;
+     let box = 100;
+     box = "a";
+
+
+
+
+     console.log(test);
+     console.log(box);
+
+     timer();
+
+     let str = some('test', 1,2,3,1,1,400);
+
+     document.querySelector('.div').innerHTML = str;
+
+
+
+     let test_2 = (x) => x + 1;
+
+     console.log(test_2(2));
+
+
+     let items = document.querySelectorAll('.items .item');
+
+     for(let it of items){
+         console.log(it);
+         it.onclick = function (e){
+            this.classList.toggle('active');
+         }
+     }
+
 
 });
+
+
+function timer(time = 60) {
+    console.log(time);
+
+}
+
+function some(name, ...marks) {
+    console.log(name);
+    console.log(marks);
+    for(let m in marks){
+        console.log(m);
+    }
+    for(let m of marks){
+        console.log(m);
+    }
+
+    let res = `${name}  ${marks}`;
+
+    return res;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
