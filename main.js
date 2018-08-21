0d7267379adcda9eae7ac8e18da4ed13f3036839
tania <tani.zhelezniak@gmail.com>
2018-08-21 11:31:16 +0300

    prototype

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ window.onload = function () {
     class Test{
         constructor(age, height){
             age: age;
+            height: height;
+        }
+        box(){
+            return "test-box";
         }
     }
 
