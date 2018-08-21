97dbca8bf2e6edf9301a8a7fe416f26aa6ea9a62
tania <tani.zhelezniak@gmail.com>
2018-08-21 14:36:11 +0300

    prototype

--- main.js
+++ main.js
@@ -2,6 +2,19 @@
 
 window.onload = function () {
 
+    let text = document.querySelector('#test');
+
+     text.onclick = function () {
+        //this.style.color = 'red';
+         this.innerHTML += 'ура';
+         this.className = 'test';
+
+     }
+
+
+
+
 
 
 }
+
