165e9547f0db48b86b14c8fd7815cb5bd24db664
tania <tani.zhelezniak@gmail.com>
2018-08-21 11:40:24 +0300

    prototype

--- main.js
+++ main.js
@@ -4,12 +4,27 @@ window.onload = function () {
 
     class Test{
         constructor(age, height){
-            age: age;
-            height: height;
+            this.age = age;
+            this.height = height;
         }
         box(){
             return "test-box";
         }
     }
+
+    let test = new Test(30, 40);
+
+    console.log(test.age);
+    console.log(test.height);
+
+    class Test_2 extends Test{
+        super();
+        constructor(){
+            this.length = 100;
+        }
+    }
+
+    let test_2 = new Test_2();
+    console.log(test_2.age);
 
 }
