700da0e8c560d54454eb6206da576ea69471885c
tania <tani.zhelezniak@gmail.com>
2018-08-22 13:17:42 +0300

    prototype

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 "use strict";
 
+
 window.onload = function () {
 
-    let text = document.querySelector('#test');
+    let prev = document.querySelector('.prev');
+    let next = document.querySelector('.next');
+    let img = document.querySelectorAll('.photos img');
+    let i = 0;
+    let len = img.length - 1;
 
-     text.onclick = function () {
-        //this.style.color = 'red';
-         this.innerHTML += 'ура';
-         this.className = 'test';
+    prev.onclick = function (e) {
+        img[i].style.display = 'none';
+        i--
+        if (i < 1) {
 
-     }
+            i = len;
+
+        }
+        img[i].style.display = 'block';
+
+    }
+
+    next.onclick = function () {
+
+        img[i].style.display = 'none';
+        i++
+        if (i >= len) {
+
+            i = 0;
+
+        }
+        img[i].style.display = 'block';
 
 
-
-
+    }
 
 
 }
