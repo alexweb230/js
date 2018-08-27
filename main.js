9a205fb6c21368215dc41a1d587a899b4f656c54
tania <tani.zhelezniak@gmail.com>
2018-08-27 12:06:44 +0300

    callback

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 "use strict";
 
 
-window.onunload = function () {
+window.onload = function () {
+
+    let item = document.querySelectorAll('.items .item');
+
+    // for(let i = 0; i < item.length; i++){
+    //      item[i].onclick = activeItem;
+    // }
+    function activeItem() {
+        this.classList.toggle('test');
+    }
 
 
+    setInterval(function () {
+        let rand = mtRand(0, item.length - 1);
+
+        activeItem.call(item[rand]);
+
+    }, 1000);
 
 
+}
 
 
+function mtRand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1));
 }
 
 
