5d3952188d09870c35e51225d00222347c4eb121
tania <tani.zhelezniak@gmail.com>
2018-10-11 14:36:23 +0300

    парктика-1

--- main.js
+++ main.js
@@ -3,7 +3,14 @@
 function sel_box() {
     let sel = document.getElementById('select').selectedIndex;
     let opt = document.getElementById('select').options;
-
     console.log("выбрана опция" + opt[sel].text);
 
+}
+
+function rang() {
+    let ran = document.getElementById('rang');
+    let text = document.getElementById('text');
+    let test = document.getElementById('test');
+    text.value = ran.value;
+    test.style.width = ran.value + "px";
 }
