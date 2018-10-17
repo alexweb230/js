9c4e478073b00174719cc5ca0532b253495f3d6e
tania <tani.zhelezniak@gmail.com>
2018-10-17 14:11:25 +0300

    парктика-1

--- main.js
+++ main.js
@@ -1,87 +1,22 @@
 "use strict";
 
-$(function () {
+function rang() {
+    let rtl = document.getElementById('rtl').value,
+        rtr = document.getElementById('rtr').value,
+        rbr = document.getElementById('rbr').value,
+        rbl = document.getElementById('rbl').value;
 
-    class Animal {
-        constructor(age) {
-            this.age = age;
-            this.length = 20;
-        }
-    }
-
-    class Cat extends Animal {
-        constructor(age) {
-            super(age);
-            this.mustache = true;
-        }
-    }
-
-    class Dog extends Cat{
-        constructor(age) {
-            super(age);
-            this.col = "white";
-        }
-    }
-    let cat = new Cat(100);
-    let dog = new Dog(400);
-
-    console.log(cat.age);
-    console.log(dog.age);
-
-     const test = 50;
-     let box = 100;
-     box = "a";
+    let ttl = document.getElementById('ttl').value,
+        ttr = document.getElementById('ttr').value,
+        tbr = document.getElementById('tbr').value,
+        tbl = document.getElementById('tbl').value;
 
 
+    let block = document.getElementById('block');
+
+    ttl = value.rtl
 
 
-     console.log(test);
-     console.log(box);
-
-     timer();
-
-     let str = some('test', 1,2,3,1,1,400);
-
-     document.querySelector('.div').innerHTML = str;
-
-
-
-     let test_2 = (x) => x + 1;
-
-     console.log(test_2(2));
-
-
-     let items = document.querySelectorAll('.items .item');
-
-     for(let it of items){
-         console.log(it);
-         it.onclick = function (e){
-            this.classList.toggle('active');
-         }
-     }
-
-
-});
-
-
-function timer(time = 60) {
-    console.log(time);
-
-}
-
-function some(name, ...marks) {
-    console.log(name);
-    console.log(marks);
-    for(let m in marks){
-        console.log(m);
-    }
-    for(let m of marks){
-        console.log(m);
-    }
-
-    let res = `${name}  ${marks}`;
-
-    return res;
 }
 
 
