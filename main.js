7399bb603abcbc0632466cac65592d8a95fdb6d7
tania <tani.zhelezniak@gmail.com>
2018-10-17 15:01:13 +0300

    парктика-3

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ function rang() {
     let rtl = document.getElementById('rtl').value,
         rtr = document.getElementById('rtr').value,
         rbr = document.getElementById('rbr').value,
-        rbl = document.getElementById('rbl').value;
+        rbl = document.getElementById('rbl').value,
 
-    let ttl = document.getElementById('ttl').value,
-        ttr = document.getElementById('ttr').value,
-        tbr = document.getElementById('tbr').value,
-        tbl = document.getElementById('tbl').value;
+        ttl = document.getElementById('ttl'),
+        ttr = document.getElementById('ttr'),
+        tbr = document.getElementById('tbr'),
+        tbl = document.getElementById('tbl'),
 
+        block = document.getElementById('block');
 
-    let block = document.getElementById('block');
-
-    ttl = value.rtl
-
+    ttl.value = rtl;
+    ttr.value = rtr;
+    tbr.value = rbr;
+    tbl.value = rbl;
+    block.style.borderRadius = rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px ';
 
 }
 
