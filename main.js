837f82ab703cbbb1a9d6aded234f90a2283ab6f6
tania <tani.zhelezniak@gmail.com>
2018-10-23 15:14:02 +0300

    animation

--- main.js
+++ main.js
@@ -1,113 +1,20 @@
-"use strict";
+let move = document.getElementById('move');
 
-$(function () {
+let box = document.getElementsByClassName('box')[0];
+let item = document.getElementsByClassName('item')[0];
+let pos = 0;
 
-    class Animal {
-        constructor(age) {
-            this.age = age;
-            this.length = 20;
-        }
-    }
+ move.onclick = function () {
+   setInterval(clearInt, 10)
+ }
 
-    class Cat extends Animal {
-        constructor(age) {
-            super(age);
-            this.mustache = true;
-        }
-    }
-
-    class Dog extends Cat{
-        constructor(age) {
-            super(age);
-            this.col = "white";
-        }
-    }
-    let cat = new Cat(100);
-    let dog = new Dog(400);
-
-    console.log(cat.age);
-    console.log(dog.age);
-
-     const test = 50;
-     let box = 100;
-     box = "a";
-
-
-
-
-     console.log(test);
-     console.log(box);
-
-     timer();
-
-     let str = some('test', 1,2,3,1,1,400);
-
-     document.querySelector('.div').innerHTML = str;
-
-
-
-     let test_2 = (x) => x + 1;
-
-     console.log(test_2(2));
-
-
-     let items = document.querySelectorAll('.items .item');
-
-     for(let it of items){
-         console.log(it);
-         it.onclick = function (e){
-            this.classList.toggle('active');
-         }
+ function clearInt() {
+     if(pos == 350){
+         clearInterval()
      }
-
-
-});
-
-
-function timer(time = 60) {
-    console.log(time);
-
-}
-
-function some(name, ...marks) {
-    console.log(name);
-    console.log(marks);
-    for(let m in marks){
-        console.log(m);
-    }
-    for(let m of marks){
-        console.log(m);
-    }
-
-    let res = `${name}  ${marks}`;
-
-    return res;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     else{
+         pos ++
+         item.style.top = pos + "px";
+         item.style.left = pos + "px";
+     }
+ }
