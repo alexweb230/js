c711f293b1c0dd04f49b2c9f89609bd123166c4b
tania <tani.zhelezniak@gmail.com>
2018-10-25 09:06:10 +0300

    keycode

--- main.js
+++ main.js
@@ -1,113 +1,24 @@
-"use strict";
+(function () {
 
-$(function () {
+    let letters = {},
+        inp = document.getElementById('input'),
+        val,
+        oldVal,
+        lastChar;
 
-    class Animal {
-        constructor(age) {
-            this.age = age;
-            this.length = 20;
+    inp.addEventListener('keyup', function (e) {
+        val = this.value;
+
+        if(e.keyCode === 8){
+            console.log("true");
         }
-    }
+        else if(e.keyCode >= 65 && e.keyCode <= 90){
 
-    class Cat extends Animal {
-        constructor(age) {
-            super(age);
-            this.mustache = true;
         }
-    }
-
-    class Dog extends Cat{
-        constructor(age) {
-            super(age);
-            this.col = "white";
-        }
-    }
-    let cat = new Cat(100);
-    let dog = new Dog(400);
-
-    console.log(cat.age);
-    console.log(dog.age);
-
-     const test = 50;
-     let box = 100;
-     box = "a";
-
-
-
-
-     console.log(test);
-     console.log(box);
-
-     timer();
-
-     let str = some('test', 1,2,3,1,1,400);
-
-     document.querySelector('.div').innerHTML = str;
-
-
-
-     let test_2 = (x) => x + 1;
-
-     console.log(test_2(2));
-
-
-     let items = document.querySelectorAll('.items .item');
-
-     for(let it of items){
-         console.log(it);
-         it.onclick = function (e){
-            this.classList.toggle('active');
-         }
-     }
-
-
-});
-
-
-function timer(time = 60) {
-    console.log(time);
-
-}
-
-function some(name, ...marks) {
-    console.log(name);
-    console.log(marks);
-    for(let m in marks){
-        console.log(m);
-    }
-    for(let m of marks){
-        console.log(m);
-    }
-
-    let res = `${name}  ${marks}`;
-
-    return res;
-}
+    })
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})();
