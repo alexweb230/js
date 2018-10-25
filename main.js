b403124fda132d444a92ec7302fc1ab9d830656c
tania <tani.zhelezniak@gmail.com>
2018-10-25 09:52:04 +0300

    keycode

--- main.js
+++ main.js
@@ -10,13 +10,32 @@
         val = this.value;
 
         if(e.keyCode === 8){
-            console.log("true");
+            lastChar = oldVal.slice(-1).toUpperCase();
+            letters[letter] = letters[letter] -1;
+            if(letters[lastChar] === 0){
+                delete letters[lastChar];
+            }
         }
         else if(e.keyCode >= 65 && e.keyCode <= 90){
+            if(val.length){
+                strToArr(val);
 
+            }
         }
-    })
+        oldVal = val;
+        console.log(letters);
+    });
 
+     function strToArr(str) {
+         let arr = str.split(""),
+             letter = arr[arr.length - 1].toUpperCase();
+         if(letters[letter]){
+             letters[letter] = letters[letter] + 1;
+         }
+         else{
+             letters[letter] = 1;
+         }
+     }
 
 
 
