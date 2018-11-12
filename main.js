df732080930e13dbe2da643a863f9311384050f4
tania <tani.zhelezniak@gmail.com>
2018-11-12 10:20:23 +0200

    init

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-let btn = document.getElementById('btn'),
-    popup = document.getElementById('mypopup'),
-    close = document.querySelector('.close');
+let btn = document.querySelectorAll('.open-popup'),
+    close = document.querySelectorAll('.close');
 
-    btn.onclick = function () {
-        popup.style.display = "block";
-    }
-    close.onclick = function () {
-        popup.style.display = "none";
-    }
+
+    btn.forEach(function (el) {
+        el.addEventListener('click', function () {
+            let popupName = el.getAttribute('data-popup');
+            document.getElementById(popupName).style.display = 'block';
+        })
+    });
+
+    close.forEach(function (el) {
+        el.addEventListener('click', function () {
+            let popup = el.closest('.popup');
+            popup.style.display = 'none';
+        })
+    });
+
     window.onclick = function (e) {
         let target = e.target;
-        if(target == popup){
-            popup.style.display = "none";
+        if(target.classList.contains('popup')){
+            target.style.display = "none";
         }
     }
 
