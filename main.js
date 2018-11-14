2458011364b82669bdc37003beca5e1ba297c1d5
tania <tani.zhelezniak@gmail.com>
2018-11-14 12:53:41 +0200

    data time

--- main.js
+++ main.js
@@ -1,113 +1,31 @@
-"use strict";
+let now = new Date();
 
-$(function () {
+let hours = now.getHours(),
+    min = now.getMinutes(),
+    sec = now.getSeconds(),
+    day = now.getDay(),
+    prepand = '',
 
-    class Animal {
-        constructor(age) {
-            this.age = age;
-            this.length = 20;
-        }
-    }
+    dayName = ['вс','пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
 
-    class Cat extends Animal {
-        constructor(age) {
-            super(age);
-            this.mustache = true;
-        }
-    }
+    // for(let i = 0; i < dayList.length; i++){
+    //     console.log(dayName[dayList[day] - 1]);
+    // }
 
-    class Dog extends Cat{
-        constructor(age) {
-            super(age);
-            this.col = "white";
-        }
-    }
-    let cat = new Cat(100);
-    let dog = new Dog(400);
-
-    console.log(cat.age);
-    console.log(dog.age);
-
-     const test = 50;
-     let box = 100;
-     box = "a";
-
-
-
-
-     console.log(test);
-     console.log(box);
-
-     timer();
-
-     let str = some('test', 1,2,3,1,1,400);
-
-     document.querySelector('.div').innerHTML = str;
-
-
-
-     let test_2 = (x) => x + 1;
-
-     console.log(test_2(2));
-
-
-     let items = document.querySelectorAll('.items .item');
-
-     for(let it of items){
-         console.log(it);
-         it.onclick = function (e){
-            this.classList.toggle('active');
-         }
-     }
-
-
-});
-
-
-function timer(time = 60) {
-    console.log(time);
-
+// console.log(now);
+// console.log(hours);
+// console.log(min);
+// console.log(sec);
+//  console.log(day);
+if(hours > 12){
+    hours -= 12;
+    prepand = "pm"
+}else if(hours == 12){
+    prepand = "am"
 }
-
-function some(name, ...marks) {
-    console.log(name);
-    console.log(marks);
-    for(let m in marks){
-        console.log(m);
-    }
-    for(let m of marks){
-        console.log(m);
-    }
-
-    let res = `${name}  ${marks}`;
-
-    return res;
+if(hours < 10){
+    hours = '0' + hours
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(`сегодня: ${dayName[day]}`);
+console.log(`время: ${hours} ${prepand}`);
 
