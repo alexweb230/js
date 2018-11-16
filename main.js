93b3c0176cc0abec75014768068b4331c36214b2
tania <tani.zhelezniak@gmail.com>
2018-11-16 13:39:48 +0200

    prototype

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
 "use strict";
 
-const TypeWriter = function (txtElement, words, wait = 3000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.wait = wait;
-    this.wordIndex = 0;
-    this.txt = '';
-    this.isDeleting = false;e3
-    this.type();
+class TypeWriter{
+    constructor(txtElement, words, wait = 3000){
+        this.txtElement = txtElement;
+        this.words = words;
+        this.wait = wait;
+        this.wordIndex = 0;
+        this.txt = '';
+        this.isDeleting = false;
+        this.type();
+    }
+    type(){
+        let speed = 200,
+            currentIndex = this.wordIndex % this.words.length,
+            currentWord = this.words[currentIndex];
+
+
+        if(this.isDeleting){
+            speed = speed / 2;
+            this.txt = currentWord.substring(0, this.txt.length - 1);
+        } else{
+
+            speed = 500;
+            this.txt = currentWord.substring(0, this.txt.length + 1);
+
+        }
+        this.txtElement.innerHTML = this.txt;
+
+        if(!this.isDeleting && this.txt === currentWord){
+            this.isDeleting = true;
+            speed = this.wait;
+        }else if(this.isDeleting && this.txt === ''){
+            this.wordIndex++;
+            this.isDeleting = false;
+        }
+
+
+        setTimeout(() => this.type(),speed)
+    }
 }
+
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', init);
 
@@ -16,11 +51,11 @@ document.addEventListener('DOMContentLoaded', init);
 function init() {
     let txtElement = document.querySelector('.text'),
         words = JSON.parse(txtElement.getAttribute('data-words')),
-        wait = txtElement.getAttribute('data-wait');````
+        wait = txtElement.getAttribute('data-wait');
 
 
 
-    console.log(words);
+        new TypeWriter(txtElement, words, wait)
 
 
 }
