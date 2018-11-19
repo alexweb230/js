bb37defba9ae64e98c072d4a6bd096ffbb677692
tania <tani.zhelezniak@gmail.com>
2018-11-19 13:10:39 +0200

    prototype

---
+++ main.js
@@ -0,0 +1,22 @@
+let forms = document.getElementsByTagName('form');
+
+for(let i = 0; i < forms.length; i++){
+   forms[i].addEventListener('submit', validator);
+}
+
+
+class Rules = {
+    email: /^\w{1,}@\w{1,}\w{2,}$/,
+}
+
+
+
+
+
+function validator() {
+
+}
+
+
+
+ console.log();
