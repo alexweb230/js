74725ff74bded525d9dec60852882c6dcd506aa0
tania <tani.zhelezniak@gmail.com>
2018-11-27 16:05:04 +0200

    validation

--- main.js
+++ main.js
@@ -1,40 +1,30 @@
 window.onload = function () {
-   let form = document.getElementById('form');
-   let formElements = form.elements;
-   let btn = document.querySelector('.btn-form');
+    let form = document.getElementById('form');
+    let formElements = form.elements;
+    let btn = document.querySelector('.btn-form');
 
 
-   btn.addEventListener('click', Validator);
+    btn.addEventListener('click', Validator);
 
-   function Validator() {
-      for(let i = 0; i < formElements.length; i++){
-        if(formElements[i].tagName == 'BUTTON' || formElements[i].tagName == 'FIELDSET'){
-           continue;
-        }
-        if(formElements[i].value == ''){
+    function Validator() {
+        for (let i = 0; i < formElements.length; i++) {
+            if (formElements[i].tagName == 'BUTTON' || formElements[i].tagName == 'FIELDSET') {
+                continue;
+            }
+            if (formElements[i].value == '') {
 
-            let tess = formElements[i].nextElementSibling.classList.add('active');
-
-
+                formElements[i].nextElementSibling.classList.add('active');
 
 
+            } else {
+                console.log('ne pusto');
+            }
 
-
-
-        } else {
-            console.log('ne pusto');
+            // console.log(formElements[i]);
         }
 
-          // console.log(formElements[i]);
-      }
 
-
-   }
-
-
-
-
-
+    }
 
 
 }
